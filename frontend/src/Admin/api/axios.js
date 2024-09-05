9bcb0d3b0b2766345fe713e1a2524222d53c4393
Nafisa Shamim Rafa <nafisashamim1999@gmail.com>
2024-09-05 16:43:56 +0600

    updated axios.js

--- frontend/src/Admin/api/axios.js
+++ frontend/src/Admin/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 //centralized axios instance to make API requests
 const api = axios.create({
 
-  baseURL:'https://collectionapp-4myu.onrender.com', // Fallback to localhost if env var is not set
+  baseURL:'https://collectionappwithjira.onrender.com', // Fallback to localhost if env var is not set
   withCredentials: true,
   
 
